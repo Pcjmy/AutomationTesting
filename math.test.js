62a48c1b292ef08ca71004afa3106785f320f922
Pcjmy <1716045191@qq.com>
2023-02-28 14:05:25 +0800

    fix: 封装测试代码

--- math.test.js
+++ math.test.js
@@ -1,13 +1,12 @@
-var result = add(3, 7);
-var expected = 10;
-
-if(result !== expected) {
-  throw Error(`3 + 7应该等于 ${expected}，但是结果却是 ${result}`);
+function expect(result) {
+  return {
+    toBe: function(actual) {
+      if(result !== actual) {
+        throw new Error('预期值和实际值不相等');
+      }
+    }
+  }
 }
 
-var result = minus(3, 3);
-var expected = 0;
-
-if(result !== expected) {
-  throw Error(`3 - 3应该等于 ${expected}，但是结果却是 ${result}`);
-}
+expect(add(3,7)).toBe(10);
+expect(minus(3,3)).toBe(0);
